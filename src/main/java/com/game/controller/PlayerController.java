8ff5aaa0d804874b148007b1b1ff6771d9131e17
deerhunter9136 <deerhunter9136@gmail.com>
2022-01-24 22:42:47 +0300

    1
    1

--- src/main/java/com/game/controller/PlayerController.java
+++ src/main/java/com/game/controller/PlayerController.java
@@ -1,95 +1,55 @@
 package com.game.controller;
 
-import com.game.entity.Profession;
-import com.game.entity.Race;
-import com.game.models.Player;
-import com.game.repository.PlayerDao;
-import com.game.service.PlayerService;
+import com.game.entity.Player;
+import com.game.service.PlayersDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/rest")
 public class PlayerController {
-    private final PlayerService playerService;
-
     @Autowired
-    public PlayerController(PlayerService playerService) {
-        this.playerService = playerService;
-    }
+    private PlayersDataService service;
 
     @GetMapping("/players")
-    public ResponseEntity<List<Player>> read(@RequestParam(required = false) String name, String title, Race race, Profession profession, Long after, Long before, Boolean banned, Integer minExperience,
-                                             Integer maxExperience, Integer minLevel, Integer maxLevel, PlayerOrder order, Integer pageNumber, Integer pageSize) {
+    public List<Player> getAll(@RequestParam Map<String, String> params) {
 
-        final List<Player> players = playerService.readAll(name, title, race, profession, after, before, banned, minExperience, maxExperience, minLevel, maxLevel, order, pageNumber, pageSize);
 
-        return players != null
-                ? new ResponseEntity<>(players, HttpStatus.OK)
-                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return service.getAll(params);
     }
 
     @GetMapping("/players/count")
-    public ResponseEntity<Long> read(@RequestParam(required = false) String name, String title, Race race, Profession profession, Long after, Long before, Boolean banned, Integer minExperience,
-                                     Integer maxExperience, Integer minLevel, Integer maxLevel) {
-
-        final Long count = playerService.count(name, title, race, profession, after, before, banned, minExperience, maxExperience, minLevel, maxLevel);
-
-        return count != null
-                ? new ResponseEntity<>(count, HttpStatus.OK)
-                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    public Integer getCount(@RequestParam Map<String, String > params) {
+        return service.getCount(params);
     }
 
-    @GetMapping(value = "/players/{id}")
-    public ResponseEntity<Player> read(@PathVariable(name = "id") long id) {
-        if (id == 0) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        final Player player = playerService.read(id);
-        return player != null
-                ? new ResponseEntity<>(player, HttpStatus.OK)
-                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    @GetMapping("/players/{id}")
+    public ResponseEntity<Player> getPlayer(@PathVariable("id") Long id) {
+        return service.getPlayerById(id);
     }
 
-    @DeleteMapping(value = "/players/{id}")
-    public ResponseEntity<?> delete(@PathVariable(name = "id") int id) {
-        final int responce = playerService.delete(id);
-        switch (responce) {
-            case 0:
-                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-            case -1:
-                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-            default:
-                return new ResponseEntity<>(HttpStatus.OK);
-        }
-
+    @PostMapping("/players")
+    public ResponseEntity<Player> createPlayer(@RequestBody Player player) {
+        if (player.getExperience() == null)
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(player);
+        Player created = service.createPlayer(player);
+        if (created == null)
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(created);
+        else return ResponseEntity.ok(created);
     }
 
-    @PostMapping(value = "/players/{id}")
-    public ResponseEntity<?> update(@PathVariable(name = "id") int id, @RequestBody Player player) {
-        if (id == 0) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        if (PlayerDao.getBase().stream().noneMatch(player1 -> player1.id == id)) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        Player updated = playerService.update(id, player.name, player.title, player.race, player.profession,
-                player.birthday, player.banned, player.experience);
-
-        return updated != null
-                ? new ResponseEntity<>(updated, HttpStatus.OK)
-                : new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    @PostMapping("/players/{id}")
+    public ResponseEntity<Player> update(@PathVariable("id") Long id, @RequestBody Player player) {
+        return service.update(id, player);
     }
 
-    @PostMapping(value = "/players")
-    public ResponseEntity<Player> create(@RequestBody Player player) {
-
-        Player created = playerService.create(player.name, player.title, player.race, player.profession, player.birthday, player.banned, player.experience);
-        return created != null
-                ? new ResponseEntity<>(created, HttpStatus.OK) :
-                new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    @DeleteMapping("/players/{id}")
+    public ResponseEntity<Long> delete(@PathVariable("id") Long id) {
+        return service.deleteById(id);
     }
-
 }
